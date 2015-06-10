b33b1d50c20e1e33d2462abe31003cc587f82a18
WaDelma <Antti@MahComputer.(none)>
2015-06-10 20:06:19 +0300

    Changed a test.

--- src/test/dim4.rs
+++ src/test/dim4.rs
@@ -58,11 +58,11 @@ fn test_4d_2th_prefilled_1th_perioditic() {
 }
 
 #[test]
-fn test_4d_8th_prefilled_4th_perioditic() {
+fn test_4d_4th_prefilled_2th_perioditic() {
     let radius = 2f64.sqrt() / 2f64;
-    test_with_seeds_prefill::<Vec4, _>(radius / 8f64, 10, true, &mut |ref mut v, i| {
+    test_with_seeds_prefill::<Vec4, _>(radius / 4f64, 1, true, &mut |ref mut v, i| {
             let rand = XorShiftRng::from_seed([i * 2 + 1, i * 1 + 1, i + 1, 2]);
-            let mut poisson = PoissonDisk::with_radius(rand, radius / 4f64, true);
+            let mut poisson = PoissonDisk::with_radius(rand, radius / 2f64, true);
             poisson.create(v);
         });
 }
